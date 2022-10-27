609e876cf836c23e78463a9e3195d5bf8035fee9
Joshua Schaaf <71152127+viabard@users.noreply.github.com>
2022-10-27 13:58:37 -0700

    added back button + js files for links
    Added a home button for some pages, and made it so I can edit an 'object' in javascript to add things to lists, instead of adding the html myself.

---
+++ links/js/creative-coding-elements.js
@@ -0,0 +1,18 @@
+const main = document.querySelector('creative-coding');
+
+
+var creative_coding_dict = {
+    'Stars': 'https://joshschaaf.com/creative-coding/stars',
+    'Bubbles': 'https://joshschaaf.com/creative-coding/bubbles',
+    'Spaghetti': 'https://joshschaaf.com/creative-coding/spaghetti'
+};
+
+var html_string = ``;
+
+Object.keys(creative_coding_dict).forEach(key => {
+    html_string += `<div class='single_link'>${key} - <a href='${creative_coding_dict[key]}' target="_blank" rel="noopener noreferrer">${creative_coding_dict[key]}</a></div><br>`;
+});
+
+print(html_string);
+
+main.innerHTML = html_string;
